902a415213523cad20a2ac617e50a575696bba26
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2019-09-20 09:24:15 +0700

    commit awal

---
+++ VR testing/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public float movementSpeed = 10;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.forward * -1 * movementSpeed * Time.deltaTime);
+        }
+            
+    }
+}
